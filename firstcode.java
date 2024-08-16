d9b5b1f61a3432285a4c43ecfc0a158a5f073b61
suryahj <surya.king0212@gmail.com>
2024-08-16 16:51:28 +0530

    making the first class in java

--- firstcode.java
+++ firstcode.java
@@ -1,3 +1,9 @@
+class firstclass
+{
+    int var1;
+    String var2;
+}
+
 class first
 {
     public static void main(String[] args) {
@@ -6,5 +12,9 @@ class first
         {
         System.out.println("hello"+" "+"this is first java code");
         }
+        firstclass ob1= new firstclass();
+        ob1.var1=44;
+        ob1.var2="surya";
+        System.out.println(ob1.var1+" "+ob1.var2);
     }
 }

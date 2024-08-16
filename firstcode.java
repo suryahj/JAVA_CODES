849b1a73ee540d478d922d824db6082f377e8b9b
suryahj <surya.king0212@gmail.com>
2024-08-16 12:45:44 +0530

    using array and loop

--- firstcode.java
+++ firstcode.java
@@ -1,6 +1,10 @@
 class first
 {
     public static void main(String[] args) {
-        System.out.print("hello"+" "+"this is first java code");
+        int [] arr = new int[5];
+        for (int i=0;i<arr.length;i++)
+        {
+        System.out.println("hello"+" "+"this is first java code");
+        }
     }
 }
